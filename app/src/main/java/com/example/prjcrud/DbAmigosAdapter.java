64bacd80c454281c73470415881a72648fb725bc
Pedro <pedhenriquecm@gmail.com>
2025-05-30 15:13:44 -0300

    CRUD Project - Android + SQLite

---
+++ app/src/main/java/com/example/prjcrud/DbAmigosAdapter.java
@@ -0,0 +1,84 @@
+package com.example.prjcrud;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DbAmigosAdapter extends RecyclerView.Adapter<DbAmigosHolder> {
+
+    private final List<DbAmigo> amigos;
+
+    public DbAmigosAdapter(List<DbAmigo> amigos) {
+        this.amigos = amigos;
+    }
+
+    public void inserirAmigo(DbAmigo amigo){
+        amigos.add(amigo);
+        notifyItemInserted(getItemCount());
+    }
+
+    public void atualizarAmigo(DbAmigo amigo){
+        amigos.set(amigos.indexOf(amigo), amigo);
+        notifyItemChanged(amigos.indexOf(amigo));
+    }
+
+
+    private Activity getActivity(View view) {
+        Context context = view.getContext();
+
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
+    }
+
+    // Este metodo retorna o layout criado pela ViewHolder, inflado numa view
+    @Override
+    public DbAmigosHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new DbAmigosHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_dados_amigo, parent, false));
+    }
+
+    // Recebe a ViewHolder e a posição da lista, de forma que um objeto da lista é recuperado pela posição e associado a ela - é o foco da ação para acontecer o processo
+
+    @Override
+    public void onBindViewHolder(DbAmigosHolder holder, int position) {
+        holder.txvNome.setText(amigos.get(position).getNome());
+        holder.txvCelular.setText(amigos.get(position).getCelular());
+        holder.txvLatitude.setText(amigos.get(position).getLatitude());
+        holder.txvLongitude.setText(amigos.get(position).getLongitude());
+        holder.btnEditar.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity activity = getActivity(v);
+                Intent intent = activity.getIntent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.putExtra("amigo", amigos.get(position));
+                activity.finish();
+                activity.startActivity(intent);
+            }
+        });
+    }
+
+    // Esta função retorna a quantidade de itens que há na lista. É importante verificar se a lista possui elementos, para não causar um erro de exceção.
+    @Override
+    public int getItemCount() {
+        return amigos != null ? amigos.size() : 0;
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/prjcrud/DbAmigosDAO.java
@@ -0,0 +1,73 @@
+package com.example.prjcrud;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbAmigosDAO {
+
+    private final String TABLE_AMIGOS = "Amigos";
+    private DbAmigosGateway gw;
+
+    public DbAmigosDAO(Context ctx){
+        gw = DbAmigosGateway.getInstance(ctx);
+    }
+
+    public boolean salvar(String nome, String celular, String latitude, String longitude, int status){
+        return salvar(0, nome, celular, latitude, longitude, status);
+    }
+
+    public boolean salvar(int id, String nome, String celular, String latitude, String longitude, int status){
+        ContentValues cv = new ContentValues();
+        cv.put("Nome", nome);
+        cv.put("Celular", celular);
+        cv.put("Latitude", latitude);
+        cv.put("Longitude", longitude);
+        cv.put("Status", status);
+        if (id > 0) {
+            return gw.getDatabase().update(TABLE_AMIGOS, cv, "ID=?", new String[]{ id + "" }) > 0;
+
+        } else {
+            return gw.getDatabase().insert(TABLE_AMIGOS, null, cv) > 0;
+        }
+    }
+
+    public List<DbAmigo> listarAmigos(){
+        List<DbAmigo> amigos = new ArrayList<>();
+        Cursor cursor = gw.getDatabase().rawQuery("SELECT * FROM Amigos", null);
+
+        while (cursor.moveToNext())
+        {
+            @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex("ID"));
+            @SuppressLint("Range") String nome = cursor.getString(cursor.getColumnIndex("Nome"));
+            @SuppressLint("Range") String celular = cursor.getString(cursor.getColumnIndex("Celular"));
+            @SuppressLint("Range") String latitude = cursor.getString(cursor.getColumnIndex("Latitude"));
+            @SuppressLint("Range") String longitude = cursor.getString(cursor.getColumnIndex("Longitude"));
+            @SuppressLint("Range") int situacao = cursor.getInt(cursor.getColumnIndex("Status"));
+            amigos.add(new DbAmigo(id, nome, celular, latitude, longitude, situacao));
+        }
+        cursor.close();
+        return amigos;
+    }
+
+    public DbAmigo ultimoAmigo(){
+        Cursor cursor = gw.getDatabase().rawQuery("SELECT * FROM Amigos ORDER BY ID DESC", null);
+        if(cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex("ID"));
+            String nome = cursor.getString(cursor.getColumnIndex("Nome"));
+            String celular = cursor.getString(cursor.getColumnIndex("Celular"));
+            String latitude = cursor.getString(cursor.getColumnIndex("Latitude"));
+            String longitude = cursor.getString(cursor.getColumnIndex("Longitude"));
+            int status = cursor.getInt(cursor.getColumnIndex("Status"));
+            cursor.close();
+            return new DbAmigo(id, nome, celular, latitude, longitude, status);
+        }
+        return null;
+    }
+
+}
+
